58231fb82d02aca835b76b7195326ee3a98554c5
An0pka <an0pka32@gmail.com>
2023-05-20 22:03:08 +0300

    1

---
+++ src/VarTask.java
@@ -0,0 +1,8 @@
+class VarTask {
+    public static void main(String[] args) {
+        byte happyValue = 6;
+
+        //Example output
+        System.out.println("happyValue = " + happyValue);
+    }
+}
