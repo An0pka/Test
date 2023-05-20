a18471020699bddc1f37506676f43a94965b49bb
An0pka <an0pka32@gmail.com>
2023-05-20 21:20:16 +0300

    test sum()

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+class Main {
+    public void printerSum(){
+        System.out.println(sum());
+    }
+    public static int sum(){
+        int newA = 1;
+        int b = 2;
+        return newA+b;
+    }
+        public static void main (String[] args) {
+            Main main = new Main();
+            main.printerSum();
+
+    }
+}
